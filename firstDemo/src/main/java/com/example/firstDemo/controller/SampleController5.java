cc3950df13462bed7aa458b575deaeeb63434267
sujeongHeo401 <gotnwjd40@gmail.com>
2022-09-30 00:53:39 +0900

    완강!
    post/get mapping // CSS 처리 // jpa 관련 warning 죽임

--- firstDemo/src/main/java/com/example/firstDemo/controller/SampleController5.java
+++ firstDemo/src/main/java/com/example/firstDemo/controller/SampleController5.java
@@ -3,6 +3,7 @@ package com.example.firstDemo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.firstDemo.dto.BookDTO;
 
@@ -19,6 +20,22 @@ public class SampleController5 {
 		model.addAttribute("bookDTO", new BookDTO());
 		
 		return "/post/SampleController5Form";
+	}
+	
+	@PostMapping("/SampleController5View")
+	public String insertView(
+			BookDTO bookDTO,
+			Model model) {
+		
+		/// bootDTO 객체 출력
+		System.out.println("-----------------");
+		System.out.println(bookDTO.getTitle());
+		System.out.println(bookDTO.getAuthor());
+		System.out.println(bookDTO.getPublisher());
+		System.out.println(bookDTO.getPublishingDate());
+		
+		model.addAttribute("heading", "Book Information");
+		return "/post/SampleController5vIEW";
 	}
 
 }
